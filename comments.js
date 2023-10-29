8ab9a5e3a03f500a24583f601a6bb2510b813334
Ashwani-Diwakar <ASHWANIDIWAKAR.210111828@gehu.ac.in>
2023-10-29 14:39:37 +0530

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,74 @@
+// Create web server
+// Express
+const express = require('express');
+const router = express.Router();
+
+// Model
+const Comment = require('../models/comment');
+
+// Routes
+// Get all comments
+router.get('/', (req, res) => {
+    Comment.find({}, (err, comments) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(comments);
+        }
+    });
+});
+
+// Get comment by id
+router.get('/:id', (req, res) => {
+    Comment.findById(req.params.id, (err, comment) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(comment);
+        }
+    });
+});
+
+// Create comment
+router.post('/', (req, res) => {
+    let comment = new Comment(req.body);
+    comment.save()
+        .then(comment => {
+            res.status(200).json({'comment': 'Added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new record');
+        });
+});
+
+// Update comment
+router.put('/:id', (req, res) => {
+    Comment.findById(req.params.id, (err, comment) => {
+        if (!comment) {
+            return next(new Error('Could not load document'));
+        } else {
+            comment.comment = req.body.comment;
+            comment.save()
+                .then(comment => {
+                    res.json('Update done');
+                })
+                .catch(err => {
+                    res.status(400).send('Update failed');
+                });
+        }
+    });
+});
+
+// Delete comment
+router.delete('/:id', (req, res) => {
+    Comment.findByIdAndRemove(req.params.id, (err, comment) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json('Remove successfully');
+        }
+    });
+});
+
+// Export router
+module.exports = router;
